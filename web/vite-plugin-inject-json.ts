fbd312240987c9b447efc663ba4eef8919fe4f47
B1A2 <B1A2@noreply.gitcode.com>
2025-04-07 19:58:02 +0800

    replace('JSON_DATA_PLACEHOLDER',scene_json_data)

--- web/vite-plugin-inject-json.ts
+++ web/vite-plugin-inject-json.ts
@@ -6,7 +6,13 @@ const injectJson: Plugin = {
     name: 'inject-json',
     transformIndexHtml(html) {
         // const jsonScript = `<script>window.jsonData = ${JSON.stringify(injectedJson)};</script>`;
-        const jsonScript = `<script src='data.js'></script>`;
+        //const jsonScript = `<script src='data.js'></script>`;
+    //     const jsonScript = `<script>
+    //     const json = JSON_DATA_PLACEHOLDER;
+    //   window.jsonData = json 
+    //   </script>
+    //   `;
+        const jsonScript = ``;
         html = html.replace('</body>', `${jsonScript}</body>`);
         return html;
     }
