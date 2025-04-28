0f93807530a26bf72858fd4843788d68d144592d
code-sunbo <bb.sunbo@huawei.com>
2025-04-28 19:08:38 +0800

    新增小红书用例

--- perf_testing/main.py
+++ perf_testing/main.py
@@ -1,8 +1,10 @@
 import os
 import time
+
+import yaml
 from xdevice.__main__ import main_process
 
-from aw.config.config import Config
+from aw.config.config import Config, ConfigError
 
 
 def load_all_testcases() -> dict:
@@ -29,16 +31,22 @@ def main():
     reports_path = os.path.join(root_path, 'reports')
     time_str = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
 
-    testcases = load_all_testcases()
-    with open(os.path.join(root_path, 'testcase.txt')) as file:
-        for line in file:
-            case_name = line.strip()
-            if not testcases[case_name]:
-                continue
-            for round in range(5):
-                case_dir = testcases[case_name]
-                output = os.path.join(reports_path, time_str, f'{case_name}_round{round}')
-                main_process(f'run -l {case_name} -tcpath {case_dir} -rp {output}')
+    all_testcases = load_all_testcases()
+    config_path = os.path.join(root_path, 'config.yaml')
+    try:
+        with open(config_path, 'r', encoding='utf-8') as f:
+            raw_data = yaml.safe_load(f)
+            for case_name in raw_data['run_testcases']:
+                if case_name not in all_testcases:
+                    continue
+                for round in range(5):
+                    case_dir = all_testcases[case_name]
+                    output = os.path.join(reports_path, time_str, f'{case_name}_round{round}')
+                    main_process(f'run -l {case_name} -tcpath {case_dir} -rp {output}')
+    except FileNotFoundError:
+        raise ConfigError(f"not found file: {config_path}")
+    except yaml.YAMLError as e:
+        raise ConfigError(f"YAML parse error: {str(e)}")
 
 
 if __name__ == "__main__":
