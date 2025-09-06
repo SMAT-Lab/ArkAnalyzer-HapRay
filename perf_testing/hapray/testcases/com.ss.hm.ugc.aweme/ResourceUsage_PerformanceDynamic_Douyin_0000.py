d43cbabbf40b876e4678caeb6dd0d4bc053ef80b
B1A2 <124698024@qq.com>
2025-09-06 17:10:55 +0800

    用例前置条件（提前关注点赞或收藏等），通过命令方式一键完成

--- perf_testing/hapray/testcases/com.ss.hm.ugc.aweme/ResourceUsage_PerformanceDynamic_Douyin_0000.py
+++ perf_testing/hapray/testcases/com.ss.hm.ugc.aweme/ResourceUsage_PerformanceDynamic_Douyin_0000.py
@@ -1,7 +1,10 @@
+import time
+
+from hypium import BY
 from hapray.core.perf_testcase import PerfTestCase
 
 
-class ResourceUsage_PerformanceDynamic_Douyin_0010(PerfTestCase):
+class ResourceUsage_PerformanceDynamic_Douyin_0000(PerfTestCase):
     def __init__(self, controllers):
         self.TAG = self.__class__.__name__
         super().__init__(self.TAG, controllers)
@@ -25,25 +28,196 @@ class ResourceUsage_PerformanceDynamic_Douyin_0010(PerfTestCase):
             # '启动被测应用'
             self.start_app()
 
-            # 浏览推荐视频, 上滑5次
+             # 浏览推荐视频, 上滑5次
             self.swipes_up(5, 1)
 
-            # 2. 抖音点击“我”，等待 2s
+            #0120前置条件
+            # 点击搜索框
+            self.driver.touch(self.convert_coordinate(1013, 179))
+            time.sleep(2)
+
+            # 搜索与辉同行
+            search_coords = self.convert_coordinate(500, 193)
+            self.driver.input_text(search_coords, '与辉同行')
+            self.driver.touch(BY.text('搜索'))
+            time.sleep(4)
+
+            #点击进入与辉同行主页
+            self.driver.touch(self.convert_coordinate(300, 350))
+            time.sleep(2)
+
+            #关注与辉同行
+            self.driver.touch(self.convert_coordinate(550, 1265))
+            time.sleep(2)
+
+            # 滑动返回
+            self.driver.swipe_to_back()
+            time.sleep(2)
+            self.driver.swipe_to_back()
+            time.sleep(2)
+            self.driver.swipe_to_back()
+            time.sleep(2)
+            self.driver.swipe_to_back()
+            time.sleep(2)
+            
+            #0110前置条件
+            #点击+
+            self.driver.touch(self.convert_coordinate(533, 2261))
+            time.sleep(2)
+            #录制视频
+            self.driver.touch(self.convert_coordinate(533, 1990))
+            time.sleep(5)
+            self.driver.touch(self.convert_coordinate(533, 1990))
+            time.sleep(1)
+            #下一步
+            self.driver.touch(BY.text('下一步'))
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(509, 1517))
+            time.sleep(1)
+
+            #设置仅自己可见
+            self.driver.touch(self.convert_coordinate(509, 1809))
+            time.sleep(1)
+            #发布
+            self.driver.touch(BY.text('发布'))
+            time.sleep(1)
+
+            #抖音点击“我”，等待 2s
             self.touch_by_text('我')
+            time.sleep(2)
+            #点击私密
+            self.touch_by_text('私密')
+            time.sleep(1)
+            #打开第一条视频
+            self.driver.touch(self.convert_coordinate(185, 1788))
+            time.sleep(1)
 
-            # 3. 抖音“我”页面点击右上角选项，等待2s
-            self.touch_by_coordinates(988, 183, 2)
+            #打开评论区
+            self.driver.touch(self.convert_coordinate(975, 1548))
+            time.sleep(1)
 
-        def step1():
-            # 1. 抖音“我”页面点击观看历史，等待2s
-            if not self.touch_by_text('观看历史', 2):
-                self.touch_by_coordinates(442, 609, 2)
+            #去评论
+            self.touch_by_text('去评论')
+            time.sleep(1)
+            #选择图片
+            self.driver.touch(self.convert_coordinate(90, 1409))
+            time.sleep(3)
+            #拍照
+            self.driver.touch(self.convert_coordinate(110, 458))
+            time.sleep(2)
+            #拍照
+            self.driver.touch(self.convert_coordinate(529, 2090))
+            time.sleep(3)
+            #确认图片
+            self.driver.touch(self.convert_coordinate(925, 2085))
+            time.sleep(3)
+            #选择图片
+            self.driver.touch(self.convert_coordinate(400, 440))
+            time.sleep(3)
+            #发送图片评论
+            self.touch_by_text('发送')
+            time.sleep(2)
 
-        def step2():
-            # 2. 观看历史上滑5次，每次等待1s;观看历史下滑5次，每次等待1s'
-            self.swipes_up(5, 1, 300)
-            self.swipes_down(5, 1, 300)
+            # 滑动返回
+            self.driver.swipe_to_back()
+            time.sleep(2)
+            self.driver.swipe_to_back()
+            time.sleep(2)
+            self.driver.swipe_to_back()
+            time.sleep(2)
+
+            #0160前置条件
+            # 点击搜索框
+            self.driver.touch(self.convert_coordinate(1013, 179))
+            time.sleep(2)
+
+            # 搜索爱吃豆腐取图看主页
+            search_coords = self.convert_coordinate(500, 193)
+            self.driver.input_text(search_coords, '爱吃豆腐取图看主页')
+            self.driver.touch(BY.text('搜索'))
+            time.sleep(2)
+
+            #点击进入爱吃豆腐取图看主页主页
+            self.driver.touch(self.convert_coordinate(327, 392))
+            time.sleep(2)
+            #点击置顶第一个视频
+            self.driver.touch(self.convert_coordinate(185, 1582))
+            time.sleep(2)
+            #收藏视频
+            self.driver.touch(self.convert_coordinate(995, 1563))
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(500, 1563))
+            time.sleep(1)
+            self.swipes_up(1, 2)
+            self.driver.touch(self.convert_coordinate(995, 1563))
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(500, 1563))
+            time.sleep(1)
+            self.swipes_up(1, 2)
+            self.driver.touch(self.convert_coordinate(995, 1563))
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(500, 1563))
+            time.sleep(1)
+            self.swipes_up(1, 2)
+            self.driver.touch(self.convert_coordinate(995, 1563))
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(500, 1563))
+            time.sleep(1)
+            self.swipes_up(1, 2)
+            self.driver.touch(self.convert_coordinate(995, 1563))
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(500, 1563))
+            time.sleep(1)
+
+            # 滑动返回
+            self.driver.swipe_to_back()
+            time.sleep(2)
+            self.driver.swipe_to_back()
+            time.sleep(2)
+            self.driver.swipe_to_back()
+            time.sleep(2)
+            
+            # 搜索Dear-迪丽热巴
+            search_coords = self.convert_coordinate(500, 193)
+            self.driver.input_text(search_coords, 'Dear-迪丽热巴')
+            self.driver.touch(BY.text('搜索'))
+            time.sleep(2)
+
+            #点击进入Dear-迪丽热巴
+            self.driver.touch(self.convert_coordinate(327, 392))
+            time.sleep(2)
+            #点击置顶第一个视频
+            self.driver.touch(self.convert_coordinate(185, 1582))
+            time.sleep(2)
+            #收藏视频
+            self.driver.touch(self.convert_coordinate(995, 1563))
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(500, 1563))
+            time.sleep(1)
+            self.swipes_up(1, 1)
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(995, 1665))
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(500, 1563))
+            time.sleep(1)
+            self.swipes_up(1, 1)
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(995, 1665))
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(500, 1563))
+            time.sleep(1)
+            self.swipes_up(1, 1)
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(995, 1665))
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(500, 1563))
+            time.sleep(1)
+            self.swipes_up(1, 1)
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(995, 1563))
+            time.sleep(1)
+            self.driver.touch(self.convert_coordinate(500, 1563))
+            time.sleep(1)
 
         start()
-        self.execute_performance_step('抖音-浏览视频场景-step1点击观看历史', 10, step1)
-        self.execute_performance_step('抖音-浏览视频场景-step2观看历史浏览', 20, step2)
+
