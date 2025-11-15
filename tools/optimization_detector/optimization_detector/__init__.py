a8673d8278fa616e667697e914d3a2ad3f8f7bb0
SunBo <bb.sunbo@huawei.com>
2025-11-15 21:29:00 +0800

    剥离optimization_detector

---
+++ tools/optimization_detector/optimization_detector/__init__.py
@@ -0,0 +1,11 @@
+"""
+优化检测器包
+用于检测二进制文件的优化级别和LTO配置
+"""
+
+__version__ = '1.0.0'
+
+from .file_info import FileCollector
+from .optimization_detector import OptimizationDetector
+
+__all__ = ['FileCollector', 'OptimizationDetector', '__version__']
