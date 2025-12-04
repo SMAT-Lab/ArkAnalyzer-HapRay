81c4772c25b90ab4d1aad806db760e2eacc5385b
B1A2 <panjianfeng4@h-partners.com>
2025-12-04 16:30:18 +0800

    用例更新。

---
+++ perf_testing/hapray/testcases/com.tencent.wechat/PerfLoad_Wechat_0090.py
@@ -0,0 +1,55 @@
+import time
+from hypium import BY
+from tensorflow import double
+
+from hapray.core.perf_testcase import PerfTestCase
+
+
+class PerfLoad_Wechat_0090(PerfTestCase):
+    def __init__(self, controllers):
+        self.TAG = self.__class__.__name__
+        super().__init__(self.TAG, controllers)
+
+        self._app_package = 'com.tencent.wechat'
+        self._app_name = '微信'
+        # 原始采集设备的屏幕尺寸（Nova 14）
+        self.source_screen_width = 1084
+        self.source_screen_height = 2412
+
+    @property
+    def app_package(self) -> str:
+        return self._app_package
+
+    @property
+    def app_name(self) -> str:
+        return self._app_name
+
+    def process(self):
+        self.start_app()
+        time.sleep(2)
+
+        # 右上角+
+        self.touch_by_coordinates(998, 176, 2)
+        self.touch_by_text('扫一扫', 5)
+        self.swipe_to_back()
+        # 右上角+
+        self.touch_by_coordinates(998, 176, 2)
+
+
+        def step1():
+            self.touch_by_text('扫一扫', 2)
+
+        self.execute_performance_step('微信-扫一扫场景-step1点击扫一扫', 60, step1)
+
+ 
+
+
+
+
+    
+        
+
+
+
+
+
